6c34d6ebff06dffa733b624ac24067d38bb2c045
kgarrity22 <garrityk@bc.edu>
2021-03-26 08:48:05 -0400

    Updates

---
+++ precache-manifest.cb742cb332dd631ddbf9be89686b90e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f3e8335d2c898f6b1f9441642b27e30",
+    "url": "/impact-heat-map/index.html"
+  },
+  {
+    "revision": "91eabc5bbdba47a94a02",
+    "url": "/impact-heat-map/static/css/2.77888103.chunk.css"
+  },
+  {
+    "revision": "2fd735c953831341a146",
+    "url": "/impact-heat-map/static/css/main.f59cf5f3.chunk.css"
+  },
+  {
+    "revision": "91eabc5bbdba47a94a02",
+    "url": "/impact-heat-map/static/js/2.7339846a.chunk.js"
+  },
+  {
+    "revision": "67cf3a927da00da6cdb5920de0d432e5",
+    "url": "/impact-heat-map/static/js/2.7339846a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fd735c953831341a146",
+    "url": "/impact-heat-map/static/js/main.e9030926.chunk.js"
+  },
+  {
+    "revision": "e72eb8970e3b41aad8cc",
+    "url": "/impact-heat-map/static/js/runtime-main.eae77db3.js"
+  }
+]);
